4bbe5c003201daa38df82a6128f531c7669493ac
Arsam Khan <102929528+ArsamK@users.noreply.github.com>
2024-08-29 05:13:18 +0000

    commit intro.js and devcontainer.json

---
+++ intro.js
@@ -0,0 +1,12 @@
+console.log("Welcome to condespace");
+
+const accountId = 23323
+let accountName = "Arsam"
+var accountType = "current account"
+accountDate = "12/june/2024"
+
+/*we avoid using var keyword for declaring vairables because it has issues with block scope and functional scope */
+
+console.table([accountId, accountName, accountType, accountDate])
+console.warn("Don't press enter!");
+
